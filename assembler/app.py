45ac574962d5c8a9b07dd5971fa7019dde666646
ParsaHaghighatgoo <time.phgg@gmail.com>
2023-12-12 02:26:47 +0330

    end of create some diffrent route :D

--- assembler/app.py
+++ assembler/app.py
@@ -3,17 +3,32 @@ import subprocess
 
 app = Flask(__name__)
 
-# app.url_for("static")
-
 @app.route('/')
 def index():
-    return render_template('coverBootstrap.html')
+    return render_template('index.html')
 
-@app.route('/assembler', methods=['POST'])
+@app.route('/parsa')
+def parsaAssembler():
+    return render_template('indexp.html')
+
+@app.route('/sina')
+def sinaAssembler():
+    return render_template('indexs.html')
+
+
+@app.route('/assemblerVS',methods=['POST'])
+def run_sinascript():
+    data = request.get_json()
+    input_data = data['input']
+    with open('AssemblyProject1.txt', 'w') as f:
+        f.write(input_data)
+    result = subprocess.check_output(['python', 'assembler.py', input_data], universal_newlines=True)
+    return jsonify({'output': result})
+
+@app.route('/assemblerVP', methods=['POST'])
 def run_script():
     data = request.json
     input_data = data['input']
-    # input_data = request.data.decode('utf-8')
     with open('AssemblyProject1.txt', 'w') as f:
         f.write(input_data)
 
